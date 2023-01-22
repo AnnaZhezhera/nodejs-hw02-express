85c27e914c60e91e32de255090663f149616ebb3
Anna Zhezhera <ianna22121996@gmail.com>
2023-01-22 23:13:30 +0200

    getAll fixes

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,20 +1,20 @@
 const { Contact } = require("../../models/contact");
 
 const getAll = async (req, res, next) => {
-  const { authUserId } = req.user;
+  const { _id } = req.user;
   const { page = 1, limit = 5, favourite } = req.query;
   const skip = (page - 1) * limit;
+  console.log("fav", favourite);
+  console.log("user", req.user);
   try {
     const result = await Contact.find(
-      favourite ? { authUserId, favourite } : { authUserId },
-      "-createdAt -updatedAt",
-      { owner: authUserId },
-      "name email phone favorite",
+      favourite ? { owner: _id, favourite } : { owner: _id },
+      "",
       {
         skip,
         limit: Number(limit),
       }
-    ).populate("owner", "authUserId email subscription");
+    ).populate("owner", "_id email subscription");
     res.json({
       status: "success",
       code: 200,
